e9c993a07a9f9fc1d419cac081308b6ac43d3180
Henry Dorfman <henrydorfman@gmail.com>
2015-11-27 22:37:14 -0800

    Add basic squanch substitutions

--- myscript.js
+++ myscript.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-  $("h1").on("click", function() {
-    console.log('fdsfdsf');
+$('h1').text('Squanchy');
+
+var squanchify = function(content) {
+  var words = ['love', 'like', 'fuck', 'the'];
+
+  words.forEach(function(word) {
+    content = content.replace(new RegExp(word, 'g'), 'squanch');
   });
-});
+
+  return content;
+};
+
+document.body.innerHTML = squanchify(document.body.innerHTML);
