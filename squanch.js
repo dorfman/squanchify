c1a996e2369ff9efddc08a755458c4cd337149b3
Henry Dorfman <henrydorfman@gmail.com>
2015-11-28 15:34:04 -0800

    Restructure passing words and squanches to forEach
    Uses an array of objects holding the array of words to be replaced and its replacement
    Function called by forEach moved inside of the outer for loop cycling through the dictionaries

--- squanch.js
+++ squanch.js
@@ -13,27 +13,27 @@ var squanchify = function(content) {
   // Each dictionary has a different tense.
   var moreWords = ['more awesome', 'messier', 'lovelier', 'better'];
   var wordly = ['lovely', 'beautiful', 'crazy', 'awesome', 'messy'];
-  var words = ['mind', 'style', 'bride', 'mess', 'feel', 'count', 'load', 'play', 'watch', 'turn', 'love', 'like', 'fuck', 'use', 'shit', 'masturbate', 'jack', 'lift', 'hell'];
+  var words = ['comment', 'mind', 'style', 'bride', 'mess', 'feel', 'count', 'load', 'play', 'watch', 'turn', 'love', 'like', 'fuck', 'use', 'shit', 'masturbate', 'jack', 'lift', 'hell'];
   var wordings = ['looking', 'messing', 'feeling', 'counting', 'loading', 'playing', 'watching', 'turning', 'loving', 'liking', 'fucking', 'using', 'jacking', 'lifting', 'masturbating'];
-  var wordeds = ['messed', 'felt', 'counted', 'loaded', 'played', 'watched', 'turned', 'loved', 'liked', 'fucked', 'used', 'jacked', 'lifted', 'masturbated'];
+  var wordeds = ['commented', 'messed', 'felt', 'counted', 'loaded', 'played', 'watched', 'turned', 'loved', 'liked', 'fucked', 'used', 'jacked', 'lifted', 'masturbated'];
 
   // To cycle through different tenses and uses of squanch
-  var squanch = 'squanchier';
-  var squanches = ['squanchier', 'squanchy', 'squanch', 'squanching', 'squanched'];
-  var dictionaries = [moreWords, wordly, words, wordings, wordeds];
-
-  // Where the squanching happens.
-  var replaceWord = function(word) {
-    content = content.replace(new RegExp('\\b' + word + '\\b', 'g'), squanch);
-    content = content.replace(new RegExp('\\b' + word.upCase() + '\\b', 'g'), squanch.upCase()); //checks to see if the first letter is capitalize
-    content = content.replace(new RegExp('\\b' + word.toUpperCase() + '\\b', 'g'), squanch.toUpperCase()); //checks to see if the word is all caps
-  };
+  var dictionary = [{words: moreWords, squanch: 'squanchier'},
+                    {words: wordly, squanch: 'squanchy'},
+                    {words: words, squanch: 'squanch'},
+                    {words: wordings, squanch: 'squanching'},
+                    {words: wordeds, squanch: 'squanched'}];
 
   // Cycles through different squanches and dictionaries
-  for (var i = 0; i < squanches.length; i++) {
-    squanch = squanches[i];
-    var dict = dictionaries[i];
-    dict.forEach(replaceWord);
+  for (var i = 0; i < dictionary.length; i++) {
+    var squanch = dictionary[i].squanch;
+    var dict = dictionary[i].words;
+
+    dict.forEach(function(word) {
+      content = content.replace(new RegExp('\\b' + word + '\\b', 'g'), squanch);
+      content = content.replace(new RegExp('\\b' + word.upCase() + '\\b', 'g'), squanch.upCase()); //checks to see if the first letter is capitalize
+      content = content.replace(new RegExp('\\b' + word.toUpperCase() + '\\b', 'g'), squanch.toUpperCase()); //checks to see if the word is all caps
+    });
   };
 
   content = wubalub(content); // Little bonus
