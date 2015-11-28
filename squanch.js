d75aee715aa6718226fd8f01a27019f013b986fa
Henry Dorfman <henrydorfman@gmail.com>
2015-11-28 14:32:23 -0800

    Add more words to be replaced
    Add words that can be replaced by 'squanchy' and 'squanchier'
    Use a for loop to cycle through different dictionaries of words to be replaced and different uses of 'squanch'

--- squanch.js
+++ squanch.js
@@ -11,10 +11,16 @@ var wubalub = function(content) {
 /* Squanch. */
 var squanchify = function(content) {
   // Each dictionary has a different tense.
-  var words = ['mess', 'feel', 'count', 'load', 'play', 'watch', 'turn', 'love', 'like', 'fuck', 'use', 'masturbate', 'jack', 'lift', 'hell'];
-  var wordings = ['messing', 'feeling', 'counting', 'loading', 'playing', 'watching', 'turning', 'loving', 'liking', 'fucking', 'using', 'jacking', 'lifting', 'masturbating'];
+  var moreWords = ['more awesome', 'messier', 'lovelier', 'better'];
+  var wordly = ['lovely', 'beautiful', 'crazy', 'awesome', 'messy'];
+  var words = ['mind', 'style', 'bride', 'mess', 'feel', 'count', 'load', 'play', 'watch', 'turn', 'love', 'like', 'fuck', 'use', 'shit', 'masturbate', 'jack', 'lift', 'hell'];
+  var wordings = ['looking', 'messing', 'feeling', 'counting', 'loading', 'playing', 'watching', 'turning', 'loving', 'liking', 'fucking', 'using', 'jacking', 'lifting', 'masturbating'];
   var wordeds = ['messed', 'felt', 'counted', 'loaded', 'played', 'watched', 'turned', 'loved', 'liked', 'fucked', 'used', 'jacked', 'lifted', 'masturbated'];
-  var squanch = 'squanch';
+
+  // To cycle through different tenses and uses of squanch
+  var squanch = 'squanchier';
+  var squanches = ['squanchier', 'squanchy', 'squanch', 'squanching', 'squanched'];
+  var dictionaries = [moreWords, wordly, words, wordings, wordeds];
 
   // Where the squanching happens.
   var replaceWord = function(word) {
@@ -23,12 +29,12 @@ var squanchify = function(content) {
     content = content.replace(new RegExp('\\b' + word.toUpperCase() + '\\b', 'g'), squanch.toUpperCase()); //checks to see if the word is all caps
   };
 
-  // Calls the replace word for the three tenses used.
-  words.forEach(replaceWord);
-  squanch = 'squanching';
-  wordings.forEach(replaceWord);
-  squanch = 'squanched';
-  wordeds.forEach(replaceWord);
+  // Cycles through different squanches and dictionaries
+  for (var i = 0; i < squanches.length; i++) {
+    squanch = squanches[i];
+    var dict = dictionaries[i];
+    dict.forEach(replaceWord);
+  };
 
   content = wubalub(content); // Little bonus
 
